64a0645371dea0dcbaeb76f019baa0fecaa0576d
Spark <piotr.rac@gmail.com>
2021-11-01 19:00:28 +0100

    1

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace BootCamp.Chapter
 {
@@ -6,7 +8,32 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            var logger = new Logger();
+            
 
+            string[] arrayS = new string[5];
+
+            foreach (var test in arrayS)
+            {
+                Console.WriteLine(test);
+            }
+
+            updateArray(arrayS);
+
+            foreach(var test in arrayS)
+            {
+                logger.Log($"{test}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void updateArray(string[] s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = "test " + i;
+            }
         }
     }
 }
