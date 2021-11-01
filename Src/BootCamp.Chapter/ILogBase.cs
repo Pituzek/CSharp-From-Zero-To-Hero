6ed75724b06b9ad32b324904deb5aa7c0d2fa88a
Spark <piotr.rac@gmail.com>
2021-11-01 20:01:31 +0100

    1

--- Src/BootCamp.Chapter/ILogBase.cs
+++ Src/BootCamp.Chapter/ILogBase.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BootCamp.Chapter
-{
-    public interface ILogBase
-    {
-        /// <summary>
-        /// Abstract class
-        /// </summary>
-        /// <param name="message"></param>
-        public void Log(ILogMessage message);
-
-    }
-}
+﻿
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -8,32 +8,6 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            var logger = new Logger();
-            
-
-            string[] arrayS = new string[5];
-
-            foreach (var test in arrayS)
-            {
-                Console.WriteLine(test);
-            }
-
-            updateArray(arrayS);
-
-            foreach(var test in arrayS)
-            {
-                logger.Log($"{test}");
-            }
-
-            Console.ReadKey();
-        }
-
-        public static void updateArray(string[] s)
-        {
-            for (int i = 0; i < s.Length; i++)
-            {
-                s[i] = "test " + i;
-            }
         }
     }
 }
