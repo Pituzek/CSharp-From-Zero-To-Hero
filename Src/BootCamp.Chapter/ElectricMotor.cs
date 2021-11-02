f2aa7e49320af899d4d4f91ccb8abf591e3f2a6e
Spark <piotr.rac@gmail.com>
2021-11-02 22:12:19 +0100

    1

---
+++ Src/BootCamp.Chapter/ElectricMotor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class ElectricMotor : IMotor
+    {
+        public string motorType { get; set; }
+        public IMovable currentPos;
+
+        public ElectricMotor(string type)
+        {
+            motorType = type;
+        }
+
+    }
+}
