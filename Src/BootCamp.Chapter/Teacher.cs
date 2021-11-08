130c34b95584da2335ac05f991b62ad0e8f55c35
Spark <piotr.rac@gmail.com>
2021-11-08 20:24:33 +0100

    Implemented Teacher.cs

---
+++ Src/BootCamp.Chapter/Teacher.cs
@@ -0,0 +1,61 @@
+﻿using BootCamp.Chapter.Hints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+
+        public Teacher()
+        {
+            Name = nameof(Teacher);
+        }
+
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+    }
+    public class HighSchoolTeacher : Teacher
+    {
+        public HighSchoolTeacher()
+        {
+            Name = nameof(HighSchoolTeacher);
+        }
+
+        public HighSchoolTeacher(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class MiddleSchoolTeacher : Teacher
+    {
+        public MiddleSchoolTeacher()
+        {
+            Name = nameof(MiddleSchoolTeacher);
+        }
+
+        public MiddleSchoolTeacher(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class UniversityTeacher : Teacher
+    {
+        public UniversityTeacher()
+        {
+            Name = nameof(UniversityTeacher);
+        }
+
+        public UniversityTeacher(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
