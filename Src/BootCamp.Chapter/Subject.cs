23ea85afc709c812ff82e68c07f10ad53ebb6263
Spark <piotr.rac@gmail.com>
2021-11-08 20:20:00 +0100

    Implemented Subject.cs

---
+++ Src/BootCamp.Chapter/Subject.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+
+        public Subject()
+        {
+            Name = nameof(Subject);
+        }
+
+        public Subject(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Maths : Subject
+    {
+        public Maths()
+        {
+            Name = nameof(Maths);
+        }
+
+        public Maths(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Art : Subject
+    {
+        public Art()
+        {
+            Name = nameof(Art);
+        }
+
+        public Art(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Music : Subject
+    {
+        public Music()
+        {
+            Name = nameof(Music);
+        }
+
+        public Music(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class PE : Subject
+    {
+        public PE()
+        {
+            Name = nameof(PE);
+        }
+
+        public PE(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class English : Subject
+    {
+        public English()
+        {
+            Name = nameof(English);
+        }
+
+        public English(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Programming : Subject
+    {
+        public Programming()
+        {
+            Name = nameof(Programming);
+        }
+
+        public Programming(string name)
+        {
+            Name = name;
+        }
+    }
+}
