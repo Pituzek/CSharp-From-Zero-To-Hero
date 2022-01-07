0906dda3fc8a3ab4a9d8c40fbf1ecce8dcedfce5
Spark <piotr.rac@gmail.com>
2022-01-07 19:35:44 +0100

    bump

---
+++ Src/BootCamp.ChapterTests/Gambling/GameComponents/DeckTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BootCamp.Chapter.Gambling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter.Gambling.Tests
+{
+    [TestClass()]
+    public class DeckTests
+    {
+        [TestMethod()]
+        public void DrawFromTopTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- Tests/BootCamp.Chapter.Tests/GameComponentsTests.cs
+++ Tests/BootCamp.Chapter.Tests/GameComponentsTests.cs
@@ -9,6 +9,49 @@ namespace BootCamp.Chapter.Tests
     public class GameComponentsTests
     {
         /// <summary>
+        ///     Check if DrawFromTop() return card from top of the stack
+        /// </summary>
+        [Fact]
+        public void Deck_DrawFromTop_return_card_from_top_of_the_deck()
+        {
+            //Arrange
+            List<Card> cardList =  CreateCards();
+            Deck testDeck = new Deck(cardList);
+            var lastIndex = cardList.Count - 1;
+
+            //Act
+            var drawCard = testDeck.DrawFromTop();
+
+            //Assert
+            Assert.Equal(cardList[lastIndex], drawCard);
+        }
+
+        [Theory]
+        [MemberData(nameof(CardDeckExpectations))]
+        public void Deck_DrawAt_return_card_at_given_index(Card.Suites suites, Card.Ranks ace, int index)
+        {
+            //Arrange
+            Deck testDeck = new Deck(CreateDeck());
+            var Card = new Card(suites, ace);
+
+            //Act
+            //Action action = () => testDeck.DrawAt(index);
+            String action = testDeck.DrawAt(index).ToString();
+
+            //Assert
+            Assert.Equal(Card.ToString(), action);
+        }
+
+        public static IEnumerable<object[]> CardDeckExpectations
+        {
+            get
+            {
+                yield return new object[] { Card.Suites.Clubs, Card.Ranks.Ace, 0 };
+                yield return new object[] { Card.Suites.Clubs, Card.Ranks.Two, 1 };
+            }
+        }
+
+        /// <summary>
         ///     Checks if exception is thrown, when provided index is bigger than current deck size
         /// </summary>
         /// <param name="index"></param>
@@ -34,5 +77,17 @@ namespace BootCamp.Chapter.Tests
             List<Card> testDeck = new List<Card> { AceClubs, TwoClubs };
             return testDeck;
         }
+
+        private static List<Card> CreateCards()
+        {
+            Card AceClubs = new Card(Card.Suites.Clubs, Card.Ranks.Ace);
+            Card TwoClubs = new Card(Card.Suites.Clubs, Card.Ranks.Two);
+            Card ThreeClubs = new Card(Card.Suites.Clubs, Card.Ranks.Three);
+            Card QueenClubs = new Card(Card.Suites.Clubs, Card.Ranks.Queen);
+            Card KingDiamonds = new Card(Card.Suites.Diamonds, Card.Ranks.King);
+            List<Card> cardList = new List<Card> { AceClubs, TwoClubs, ThreeClubs, QueenClubs, KingDiamonds };
+            return cardList;
+        }
+
     }
 }
